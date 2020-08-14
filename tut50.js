6ed474f23fbdd7b4dca3e2496f2a0b4d7512485b
Ashish <ashishsatav968@gmail.com>
2020-08-14 16:47:13 +0530

    JS added

---
+++ tut50.js
@@ -0,0 +1,20 @@
+console.log("this is 50 tutorial")
+
+function greet(name,greetText) {
+    let name1="Name1";
+    console.log(greetText+" "+name);
+    console.log(name+ "is a good boy");
+}
+let name="Arnav";
+let name1="AJIT";
+let name2="AASHISH";
+let name3="ArOHI";
+let greetText = "Good Morning";
+greet(name,greetText);
+greet(name1,greetText);
+greet(name2,greetText);
+greet(name3,greetText);
+// console.log(name+"is a good boy")
+// console.log(name1+"is a good boy")
+// console.log(name2+"is a good boy")
+// console.log(name3+"is a good boy")
